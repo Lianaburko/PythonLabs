8df6ca14f4068da9f8ce3dd05db656b172b877ad
Lianaburko <burkoliana@gmail.com>
2022-03-19 00:52:59 +0100

    fix input

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,12 +1,22 @@
+from unicodedata import digit
 import lab1_task
 
 def main():
-    print('add your k')
-    k = int(input())
-    print('add your n')
-    n = int(input())
-    s = input().lower()
+    print('Do you want to change K? Default is 4. If no, enter anything you want')
+    k = input()
+    if k is digit:
+        k = int(k)
+    else:
+        k = 4    
 
+    print('Do you want to change N? Default is 10. If no, enter anything you want')
+    n = input()
+    if n is digit:
+        n = int(n)
+    else: 
+        n = 10
+
+    s = input().lower()
     numbers = lab1_task.amount_of_words_in_sentences(s) #list of words_amount in each sentence
     word_list = lab1_task.words_in_text(s) # list of words
     dicti = lab1_task.amount_of_diff_words(word_list) # dictionary of words
