ef8fdb0011b5be0daaa8103ad94846b395f43458
Lianaburko <burkoliana@gmail.com>
2022-05-06 14:55:41 +0200

    add dict and list

---
+++ Lab2/json_deserializator.py
@@ -0,0 +1,93 @@
+def deserialize_json(s):
+
+    index = 0 
+
+    if s[index] in '-0123456789':
+        obj = ''
+        check_float = False
+        while s[index] != ',' and s[index] != '\n' and s[index] != ' ' and s[index] != ']' and s[index] != '}':
+            obj += s[index]
+            if index < len(s)-1:
+                index += 1 
+                if s[index] == '.':
+                    check_float = True
+            else:
+                break
+        s = s[index::]
+        if check_float:
+            res = float(obj)
+        else:
+            res = int(obj)
+        return s, res
+    
+    elif s[index] == '"':
+        index += 1
+        obj = ''
+        while s[index] != '"':
+            obj += s[index]
+            index += 1
+        s = s[(index+1)::]
+        res = obj 
+        return s, res
+
+
+    elif s[index] == '[':  #res - all list #s - string after ]
+        res = ''
+        weight = 0
+        check = 'end'
+        while check != '0]':
+            res += s[index] 
+            if s[index] == ']':
+                weight += 1
+            elif s[index] == '[':
+                weight -= 1
+            check = str(weight) + s[index]
+            index += 1
+        s = s[index::]    
+        result = list_deserialization(res)
+        return s, result
+
+
+    elif s[index] == '{':
+        res = ''
+        weight = 0
+        check = 'end'
+        while check != '0}':
+            res += s[index] 
+            if s[index] == '}':
+                weight += 1
+            elif s[index] == '{':
+                weight -= 1
+            check = str(weight) + s[index]
+            index += 1
+        s = s[index::]   
+        result = dict_deserialization(res)
+        return s, result
+
+
+def dict_deserialization(res):
+    result = {}
+    
+    while res != '}':
+        if res[0] in "{},\n []":
+            res = res[1::]     
+        elif res[0] == '"':
+            res, key = deserialize_json(res)
+            res = res[1:]
+            res, value = deserialize_json(res)
+            result[key] = value
+
+    return result
+
+def list_deserialization(res): # res - current list, 
+    result = []
+    res = res[1::]
+    while res != ']':
+        if res[0] in ",\n ":
+            res = res[1::]     
+        else:
+            res, obj = deserialize_json(res)
+            result.append(obj)
+
+    return result
+
